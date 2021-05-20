32866404799139048db44eee2e5d22c15e14cf4d
tirumalaraju <sireesha@zelarsoft.com>
2021-05-20 21:53:39 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,7 +3,7 @@ def nexus(COMPONENT) {
   def get_branch_exec=sh(returnStdout: true, script: get_branch)
   def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-  command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://172.31.11.104:8081/repository/${COMPONENT}/${FILENAME}"
+  command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://172.31.3.145:8081/repository/${COMPONENT}/${FILENAME}"
   def execute_state=sh(returnStdout: true, script: command)
 }
 
@@ -12,8 +12,8 @@ def make_artifacts(APP_TYPE, COMPONENT) {
   def get_branch_exec=sh(returnStdout: true, script: get_branch)
   println("abc${get_branch_exec}abc")
   def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-  if(APP_TYPE == "NODEJS") {
-    command = "cd static && zip -r ${FILENAME} *"
+  if(APP_TYPE == "NGINX") {
+    command = "cd static && zip -r ../${FILENAME} *"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "NODEJS") {
@@ -25,7 +25,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "GOLANG") {
-    command = "zip -r ${FILENAME} login-ci main.go"
+    command = "zip -r ${FILENAME} login-ci main.go user.go tracing.go"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   }
@@ -44,5 +44,5 @@ def code_build(APP_TYPE, COMPONENT) {
     command = "go build"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
-}
+  }
 }

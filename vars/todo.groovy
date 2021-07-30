6fc58a9e6148ed7202c5bdb6ab3742763d1c86a0
tirumalaraju <sireesha@zelarsoft.com>
2021-07-30 17:41:51 +0530

    kk

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -25,6 +25,7 @@ def call(Map params = [:]) {
 
       stage('Build Code & Install Dependencies') {
         steps {
+          sh 'env'
           script {
             build = new nexus()
             build.code_build("${APP_TYPE}", "${COMPONENT}")
@@ -48,6 +49,16 @@ def call(Map params = [:]) {
             prepare = new nexus()
             prepare.nexus(COMPONENT)
           }
+        }
+      }
+
+      stage('Deploy to Dev Env') {
+        steps {
+          script {
+            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+          }
+          build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
         }
       }
 

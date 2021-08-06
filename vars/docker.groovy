2b09113feec40091083cda1f8ee6a9d4f05b84a4
tirumalaraju <sireesha@zelarsoft.com>
2021-08-06 11:36:42 +0530

    cdc

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -33,7 +33,7 @@ def call(Map params = [:]) {
           }
           sh '''
             aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 451563709560.dkr.ecr.us-east-1.amazonaws.com
-            docker build -t 451563709560.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec}
+            docker build -t 451563709560.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec} .
             '''
         }
       }
